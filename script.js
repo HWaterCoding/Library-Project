7c8ce52cdc895d23f9b56e6c013ad72a3ccd8e04
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-02-10 17:22:15 -0500

    Re-factor Book constructor to class syntax Add toggleRead method to Book class
    Create Library class and store addBookToLibrary
    Add sortLibrary function and getter to Library class

--- script.js
+++ script.js
@@ -13,10 +13,7 @@ const removeBookModal = document.getElementById("modal2");
 
 
 
-//library array to hold all book objects
-// let myLibrary = [];
-
-
+//Book class to handle individual book logic
 class Book{
     constructor(title, author, genre, pages, read){
         this.id = crypto.randomUUID();
@@ -28,7 +25,16 @@ class Book{
         this.read = read;  
     }
 
-    myLibrary = [];
+    toggleRead(){
+        this.read = !this.read;
+    }
+}
+
+//Library class to handle the entire array fo books logic
+class Library{
+    constructor(){
+        this.library = [];
+    }
 
     addBookToLibrary(){
         const titleValue = titleInput.value;
@@ -37,102 +43,73 @@ class Book{
         const pagesValue = pagesInput.value;
         const checkboxValue = readCheckbox.checked;
         const newBook = new Book(titleValue, authorValue, genreValue, pagesValue, checkboxValue);
-        myLibrary.push(newBook);
-        sortArray();
+        library.push(newBook);
+        sortLibrary();
     }
 
-    displayBooks(){
-        bookGrid.innerHTML = "";
-        for(const bookObj of myLibrary){
+    sortLibrary(){
+        const selectedValue = sortSelect.value;
+        switch(selectedValue){
+        case "newToOld":
+            library.sort((a, b) => b.createdDate - a.createdDate);
+        break ;
+            
+        case "oldToNew":
+            library.sort((a, b) => a.createdDate - b.createdDate);
+        break ;
 
-            const bookElement = document.createElement("div");
-            bookElement.classList.add("book");
-            bookElement.dataset.id = bookObj.id;
+        case "titlea2z":
+            library.sort((a, b) => a.title.localeCompare(b.title));
+        break ;
 
-            const titleh1 = document.createElement("h1");
-            titleh1.textContent = `${bookObj.title}`;
-            bookElement.appendChild(titleh1);
+        case "titlez2a":
+            library.sort((a, b) => b.title.localeCompare(a.title));
+        break ;
 
-            const authorh2 = document.createElement("h2");
-            authorh2.textContent = `By: ${bookObj.author}`;
-            bookElement.appendChild(authorh2);
+        case "authora2z":
+            library.sort((a, b) => a.author.localeCompare(b.author));
+        break ;
 
-            const genreh2 = document.createElement("h2");
-            genreh2.textContent = `Genre: ${bookObj.genre}`;
-            bookElement.appendChild(genreh2);
+        case "authorz2a":
+            library.sort((a, b) => b.author.localeCompare(a.author));
+        break ;
 
-            const pagesP = document.createElement("p");
-            pagesP.textContent = `Pages: ${bookObj.pages}`;
-            bookElement.appendChild(pagesP);
+        case "genrea2z":
+            library.sort((a, b) => a.genre.localeCompare(b.genre));
+        break ;
 
-            const readButton = document.createElement("button");
-            if(bookObj.read){
-                readButton.classList.add("readButton")
-                readButton.textContent = "Read";
-            } else {
-                readButton.classList.add("notReadButton")
-                readButton.textContent = "Unread";
-            }   
-            readButton.addEventListener("click", () =>{
-                if(readButton.classList.contains("readButton")){
-                    readButton.classList.remove("readButton");
-                    readButton.classList.add("notReadButton");
-                    readButton.textContent = "Unread";
-                    bookObj.read = false;
-                } else{
-                    readButton.classList.remove("notReadButton");
-                    readButton.classList.add("readButton");
-                    readButton.textContent = "Read";
-                    bookObj.read = true;
-                }
-            });
-            bookElement.appendChild(readButton);
+        case "genrez2a":
+            library.sort((a, b) => b.genre.localeCompare(a.genre));
+        break;
 
-            const removeBookBtn = document.createElement("button");
-            removeBookBtn.classList.add("removeBookBtn");
-            removeBookBtn.textContent = "Remove Book";
-            removeBookBtn.addEventListener("click", (event) =>{
-                const bookElement = event.target.closest(".book");
-                idToDelete = bookElement.dataset.id;
+        case "read":
+            library.sort((a, b) => b.read - a.read);
+        break ;
 
-                overlay.style.display = "flex";
-                addBookModal.style.display = "none";
-                removeBookModal.style.display = "block";
-            })
-            bookElement.appendChild(removeBookBtn);
-
-            bookGrid.appendChild(bookElement);
+        case "unread":
+            library.sort((a, b) => a.read - b.read);
+        break ;        
+        default:
+            library.sort((a, b) => b.createdDate - a.createdDate);
+        break;
         }
+    }
+
+    get library(){
+        return this.library; 
+    }
+}
+
+class LibraryUI{
+    constructor(){
+
     }
 }
 
 
-//Book Object Constructor Function
-// function Book(title, author, genre, pages, read){
-//     this.id = crypto.randomUUID();
-//     this.createdDate = Date.now();
-//     this.title = title;
-//     this.author = author;
-//     this.genre = genre;
-//     this.pages = pages;
-//     this.read = read;
-// }
-
-// Function to create New books and store in myLibrary array
-// function addBookToLibrary(){
-//     const titleValue = titleInput.value;
-//     const authorValue = authorInput.value;
-//     const genreValue = genreInput.value;
-//     const pagesValue = pagesInput.value;
-//     const checkboxValue = readCheckbox.checked;
-//     const newBook = new Book(titleValue, authorValue, genreValue, pagesValue, checkboxValue);
-//     myLibrary.push(newBook);
-//     sortArray();
-// }
-
-
 
 //function to clear inputs on form when book added or modal closed
+
 function clearForm(){
     titleInput.value = "";
     authorInput.value = "";
@@ -168,69 +145,69 @@ form.addEventListener("submit", (event) => {
 
 
 //Loops through library array, creates book elements for objects, displays those books on page
-// function displayBooks(){
-//     bookGrid.innerHTML = "";
-//     for(const bookObj of myLibrary){
+function displayBooks(){
+    bookGrid.innerHTML = "";
+    for(const bookObj of myLibrary){
 
-//         const bookElement = document.createElement("div");
-//         bookElement.classList.add("book");
-//         bookElement.dataset.id = bookObj.id;
+        const bookElement = document.createElement("div");
+        bookElement.classList.add("book");
+        bookElement.dataset.id = bookObj.id;
 
-//         const titleh1 = document.createElement("h1");
-//         titleh1.textContent = `${bookObj.title}`;
-//         bookElement.appendChild(titleh1);
+        const titleh1 = document.createElement("h1");
+        titleh1.textContent = `${bookObj.title}`;
+        bookElement.appendChild(titleh1);
 
-//         const authorh2 = document.createElement("h2");
-//         authorh2.textContent = `By: ${bookObj.author}`;
-//         bookElement.appendChild(authorh2);
+        const authorh2 = document.createElement("h2");
+        authorh2.textContent = `By: ${bookObj.author}`;
+        bookElement.appendChild(authorh2);
 
-//         const genreh2 = document.createElement("h2");
-//         genreh2.textContent = `Genre: ${bookObj.genre}`;
-//         bookElement.appendChild(genreh2);
+        const genreh2 = document.createElement("h2");
+        genreh2.textContent = `Genre: ${bookObj.genre}`;
+        bookElement.appendChild(genreh2);
 
-//         const pagesP = document.createElement("p");
-//         pagesP.textContent = `Pages: ${bookObj.pages}`;
-//         bookElement.appendChild(pagesP);
+        const pagesP = document.createElement("p");
+        pagesP.textContent = `Pages: ${bookObj.pages}`;
+        bookElement.appendChild(pagesP);
 
-//         const readButton = document.createElement("button");
-//         if(bookObj.read){
-//             readButton.classList.add("readButton")
-//             readButton.textContent = "Read";
-//         } else {
-//             readButton.classList.add("notReadButton")
-//             readButton.textContent = "Unread";
-//         }   
-//         readButton.addEventListener("click", () =>{
-//             if(readButton.classList.contains("readButton")){
-//                 readButton.classList.remove("readButton");
-//                 readButton.classList.add("notReadButton");
-//                 readButton.textContent = "Unread";
-//                 bookObj.read = false;
-//             } else{
-//                 readButton.classList.remove("notReadButton");
-//                 readButton.classList.add("readButton");
-//                 readButton.textContent = "Read";
-//                 bookObj.read = true;
-//             }
-//         });
-//         bookElement.appendChild(readButton);
+        const readButton = document.createElement("button");
+        if(bookObj.read){
+            readButton.classList.add("readButton")
+            readButton.textContent = "Read";
+        } else {
+            readButton.classList.add("notReadButton")
+            readButton.textContent = "Unread";
+        }   
+        readButton.addEventListener("click", () =>{
+            if(readButton.classList.contains("readButton")){
+                readButton.classList.remove("readButton");
+                readButton.classList.add("notReadButton");
+                readButton.textContent = "Unread";
+                bookObj.read = false;
+            } else{
+                readButton.classList.remove("notReadButton");
+                readButton.classList.add("readButton");
+                readButton.textContent = "Read";
+                bookObj.read = true;
+            }
+        });
+        bookElement.appendChild(readButton);
 
-//         const removeBookBtn = document.createElement("button");
-//         removeBookBtn.classList.add("removeBookBtn");
-//         removeBookBtn.textContent = "Remove Book";
-//         removeBookBtn.addEventListener("click", (event) =>{
-//             const bookElement = event.target.closest(".book");
-//             idToDelete = bookElement.dataset.id;
+        const removeBookBtn = document.createElement("button");
+        removeBookBtn.classList.add("removeBookBtn");
+        removeBookBtn.textContent = "Remove Book";
+        removeBookBtn.addEventListener("click", (event) =>{
+            const bookElement = event.target.closest(".book");
+            idToDelete = bookElement.dataset.id;
 
-//             overlay.style.display = "flex";
-//             addBookModal.style.display = "none";
-//             removeBookModal.style.display = "block";
-//         })
-//         bookElement.appendChild(removeBookBtn);
+            overlay.style.display = "flex";
+            addBookModal.style.display = "none";
+            removeBookModal.style.display = "block";
+        })
+        bookElement.appendChild(removeBookBtn);
 
-//         bookGrid.appendChild(bookElement);
-//     }
-// }
+        bookGrid.appendChild(bookElement);
+    }
+}
 
 
 //global variable to remain null until removeBookBtn is pressed
@@ -264,56 +241,9 @@ close2ndModalBtn.addEventListener("click", () =>{
 //Drop-down select list to sort library. Call sort function then re-display books.
 const sortSelect = document.getElementById("sortBy");
 sortSelect.addEventListener("change", () =>{
-    sortArray(sortSelect.value);
+    sortLibrary(sortSelect.value);
     displayBooks();
 });
 
-//function to sort book objects based on criteria selected from drop-list
-function sortArray(){
-    const selectedValue = sortSelect.value;
-    switch(selectedValue){
-    case "newToOld":
-        myLibrary.sort((a, b) => b.createdDate - a.createdDate);
-    break ;
-        
-    case "oldToNew":
-        myLibrary.sort((a, b) => a.createdDate - b.createdDate);
-    break ;
 
-    case "titlea2z":
-        myLibrary.sort((a, b) => a.title.localeCompare(b.title));
-    break ;
-
-    case "titlez2a":
-        myLibrary.sort((a, b) => b.title.localeCompare(a.title));
-    break ;
-
-    case "authora2z":
-        myLibrary.sort((a, b) => a.author.localeCompare(b.author));
-    break ;
-
-    case "authorz2a":
-        myLibrary.sort((a, b) => b.author.localeCompare(a.author));
-    break ;
-
-    case "genrea2z":
-        myLibrary.sort((a, b) => a.genre.localeCompare(b.genre));
-    break ;
-
-    case "genrez2a":
-        myLibrary.sort((a, b) => b.genre.localeCompare(a.genre));
-    break;
-
-    case "read":
-        myLibrary.sort((a, b) => b.read - a.read);
-    break ;
-
-    case "unread":
-        myLibrary.sort((a, b) => a.read - b.read);
-    break ;        
-    default:
-        myLibrary.sort((a, b) => b.createdDate - a.createdDate);
-    break;
-    }
-}
 
